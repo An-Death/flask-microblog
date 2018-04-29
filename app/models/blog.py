55648d30957a543c05b4fbad7605323b63eb9bff
An-Death <saenarayoursorrow@gmail.com>
2018-04-29 20:37:27 +0300

    Flask models fix repr

--- app/models/blog.py
+++ app/models/blog.py
@@ -10,28 +10,37 @@ from app import db
 class MetaModel:
     def __repr__(self, **kwargs):
         class_name = self.__class__.__name__
-        return f'<{class_name}: {" ".join(map(":".join, kwargs.items()))}>'
+        return f'<{class_name}: {"|".join(map(":".join, kwargs.items()))}>'
 
 
-class ModelMixin(MetaModel):
+class Model(MetaModel):
     id = db.Column(db.Integer, primary_key=True)
 
     class Meta:
         repr_fields = ['id']
 
     def __repr__(self):
-        kwargs = {field: getattr(self, field) for field in self.Meta.repr_fields}
+        kwargs = {field: str(getattr(self, field)) for field in self.Meta.repr_fields}
         return super().__repr__(**kwargs)
 
+    def __str__(self):
+        return f'"{getattr(self, self.Meta.repr_fields[0])}"'
 
-class User(db.Model, ModelMixin):
+
+class User(Model, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    class Meta:
+        repr_fields = ['username']
 
-class Post(db.Model, ModelMixin):
+
+class Post(Model, db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    class Meta:
+        repr_fields = ['author', 'body']
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 class Config:
     PROD = 'production'
     TEST = 'test'
