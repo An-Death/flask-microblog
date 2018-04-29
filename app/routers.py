a448df7a7dc84859324da35c2cd78217da643b95
An-Death <saenarayoursorrow@gmail.com>
2018-04-29 15:19:57 +0300

    Flask index add posts

--- app/routers.py
+++ app/routers.py
@@ -3,12 +3,19 @@
 from flask import render_template
 
 from app import app
-from app.helpers.decorators import title
+from app import helpers
 
 
 @app.route('/')
 @app.route('/index.html')
-@title(title_name='Main')
+@helpers.title(title_name='Home')
 def hello_world(*args, **kwargs):
-    user = {'name': 'Name'}
+    user = {'username': 'Name'}
+    posts = [
+                {
+                    'author': {'username': 'Name'},
+                    'body': 'Some message'
+
+                }
+            ] * 10
     return render_template('index.html', __title__=kwargs['__title__'], **locals())
