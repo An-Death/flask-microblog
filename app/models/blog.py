c189c33ac9aead961ea24de9affb4e738ca82b1a
An-Death <saenarayoursorrow@gmail.com>
2018-05-03 21:39:47 +0300

    Flask models followers and db migration
    Signed-off-by: An-Death <saenarayoursorrow@gmail.com>

--- app/models/blog.py
+++ app/models/blog.py
@@ -33,6 +33,12 @@ class Model(MetaModel, db.Model):
         return f'"{getattr(self, self.Meta.repr_fields[0])}"'
 
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+                     )
+
+
 class User(Model, UserMixin):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -40,6 +46,13 @@ class User(Model, UserMixin):
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     class Meta:
         repr_fields = ['username']
